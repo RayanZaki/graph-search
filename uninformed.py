f6bd46c18702cc06aebda407407fcdd36d8c0018
RayanZaki <rayan.hassici@ensia.edu.dz>
2023-03-26 23:41:45 +0100

    added the depth first search and modified get Path
    Signed-off-by: RayanZaki <rayan.hassici@ensia.edu.dz>

--- uninformed.py
+++ uninformed.py
@@ -3,29 +3,26 @@ from graph import Graph
 from node import Node
 
 
-def get_path(node, graph, p=Path(recursive=True)):
+def get_path(node, graph, p=None):
     """
     Recursive Function to climb up the Parents of the
-    Goal node and return the The Goal Path
-    Parameters
-    ----------
-    node
-    graph
-    p
-
-    Returns
-    -------
-
+    Goal node and return the Goal Path
     """
+    # Make the Path follow the graph
+    #  weighted => Path weighted
+    if p is None:
+        p = Path(recursive=True)
+
     p.weighted = graph.weighted
-    print(p.weighted)
-    if node.parent is None:
-        return p
+
+    if node.parent is None:  # ie Reached the root
+        return p  # Return the path
+    print(node.parent, node)
     p.insert(node.parent, node, graph.get(node.parent, node))
     return get_path(node.parent, graph, p)
 
 
-def breadth_first(node: Node, goal: Node, g: Graph):
+def breadth_first(node: any, goal: any, g: Graph):
     """
     Parameters
     ----------
@@ -33,45 +30,51 @@ def breadth_first(node: Node, goal: Node, g: Graph):
     goal: The Gaol State, It also can be of any type
     g: The Graph that relates both states
     """
-
     # Adjusting Parameters to be of type Node
     node = Node.params_to_node(node)
     goal = Node.params_to_node(goal)
 
-    # Initialize the goal path
-    goal_path = Path(weighted=g.weighted)
-
+    if node == goal:
+        return Path()
     # initialize the visited array
     visited = [node]
     # initialize the search FIFO queue
     queue = [node]
 
-    p = None
-    # initialize the parent to None
     while queue:  # is not empty
         s = queue.pop(0)
-        # print(s)
-        # print("parent: ", s.parent)
-        # print("p: ", p)
+        for i in g.adjacent(s):
+            i.parent = s    # set the parent node attribute on each child
+            if i == goal:
+                return get_path(i, g)
+            if i not in visited:
+                queue.append(i)
+                visited.append(i)
+    return Path()
 
-        # if p != s.parent:
-        # p = s.parent
-        # try:
-        # print("inseted: ", p, s)
-        # goal_path.insert(p, s)
-        # except IncompletePath:
-        #     pass
-        # else:
-        #     print("remove last")
-        #     goal_path.remove_last()
 
+def depth_first(node: any, goal: any, g: Graph):
+    """
+    Apply depth first search on Graph g starting from node
+    up to goal
+    """
+
+    # Adjusting Parameters to be of type Node
+    node = Node.params_to_node(node)
+    goal = Node.params_to_node(goal)
+
+    if node == goal:
+        return Path()
+    # initialize the visited array
+    visited = [node]
+    # initialize the search LIFO queue
+    queue = [node]
+
+    while queue:  # is not empty
+        s = queue.pop()
         for i in g.adjacent(s):
             i.parent = s
             if i == goal:
-                # try:
-                #     goal_path.insert(s, i)
-                # except IncompletePath:
-                #     pass
                 return get_path(i, g)
             if i not in visited:
                 queue.append(i)
